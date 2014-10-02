83698c86ced3b3122502ff522a565273ac990b15
Marius Volkhart <Marius@volkhart.com>
2014-10-02 17:13:59 -0500

    Complete Protos for Admin funcitonality and then some
    - Protos for Trucks, Menu, MenuAdmin, Healthcheck, and Error

--- generate.sh
+++ generate.sh
@@ -3,9 +3,22 @@
 # Clean
 rm -rf build
 
-# Android
-java -jar wire-compiler-1.5.3-SNAPSHOT-jar-with-dependencies.jar --proto_path=com --java_out=build/android --service_writer=com.squareup.wire.OurRetrofitServiceWriter truckmuncher/api/v1/healthcheck.proto
+# Need to create output directories b/c protoc can't
+mkdir -p build/iOS
+mkdir -p build/python
 
-# API
-java -jar wire-compiler-1.5.3-SNAPSHOT-jar-with-dependencies.jar --proto_path=com --java_out=build/api --service_writer=com.squareup.wire.JerseyServiceWriter truckmuncher/api/v1/healthcheck.proto
-protoc -I=. --java_out=build/api com/truckmuncher/api/v1/healthcheck.proto
+for file in $(find com/truckmuncher/api -name '*.proto');
+do
+	
+	# Android
+	java -jar wire-compiler-1.5.3-SNAPSHOT-for-android.jar --java_out=build/android --service_writer=com.squareup.wire.OurRetrofitServiceWriter $file
+
+	# API
+	java -jar wire-compiler-1.5.3-SNAPSHOT-for-api.jar --java_out=build/api --service_writer=com.squareup.wire.JerseyServiceWriter $file;
+
+	# iOS
+	protoc -I=. $file --swift_out="build/iOS"
+
+	# Api tests
+	protoc -I=. $file --python_out="build/python"
+done
